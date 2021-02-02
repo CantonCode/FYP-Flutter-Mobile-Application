a2be460a5aec039f2e8194e4991c8adb0b27361d
Conor Canton <conorstreete@gmail.com>
2021-02-02 08:50:43 +0000

    added basic drawer

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:fyp_application/auth_service.dart';
+
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,9 +11,7 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       title: "REGISTER",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("REGISTER"),
-        ),
+        appBar: AppBar(),
         body: HomeScreen(),
       ),
     );
@@ -31,26 +30,54 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return MaterialApp(
         home: Scaffold(
+            drawer: Drawer(
+              child: ListView(
+                children: <Widget>[
+                  UserAccountsDrawerHeader(
+                    accountName: Text("Ashish Rawat"),
+                    accountEmail: Text("ashishrawat2911@gmail.com"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundColor:
+                          Theme.of(context).platform == TargetPlatform.iOS
+                              ? Colors.blue
+                              : Colors.white,
+                      child: Text(
+                        "A",
+                        style: TextStyle(fontSize: 40.0),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text("Ttem 1"),
+                    trailing: Icon(Icons.arrow_forward),
+                  ),
+                  ListTile(
+                    title: Text("Item 2"),
+                    trailing: Icon(Icons.arrow_forward),
+                  ),
+                ],
+              ),
+            ),
             body: Center(
                 child: Container(
                     child: Column(children: <Widget>[
-      SizedBox(
-        height: 16,
-      ),
-      Text("HOME PAGE"),
-      SizedBox(
-        height: 16,
-      ),
-      Text("WELCOME: " + firebaseUser.email.toString()),
-      SizedBox(
-        height: 16,
-      ),
-      RaisedButton(
-        onPressed: () {
-          context.read<AuthService>().signOut();
-        },
-        child: Text("Sign Out"),
-      )
-    ])))));
+              SizedBox(
+                height: 16,
+              ),
+              Text("HOME PAGE"),
+              SizedBox(
+                height: 16,
+              ),
+              Text("WELCOME: " + firebaseUser.email.toString()),
+              SizedBox(
+                height: 16,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  context.read<AuthService>().signOut();
+                },
+                child: Text("Sign Out"),
+              )
+            ])))));
   }
 }
